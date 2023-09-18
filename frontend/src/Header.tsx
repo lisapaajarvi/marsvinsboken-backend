99cab49c46e5db5d477014e7631f99a9168a7f85
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2023-09-18 10:10:46 +0200

    added tailwind styling to header

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,18 +1,18 @@
 export default function Header() {
   return (
     //stylingexempel med objektvariabel
-    <header style={headerStyle}>
-      <h1>Marsvinsboken</h1>
+    <header className="w-full bg-violet-800 text-cyan-200 flex justify-center items-center h-24">
+      <h1 className="text-md lg:text-4xl">Marsvinsboken</h1>
     </header>
   );
 }
 
-const headerStyle = {
-  width: "100%",
-  height: "6rem",
-  backgroundColor: "darkblue",
-  color: "lightblue",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
+// const headerStyle = {
+//   width: "100%",
+//   height: "6rem",
+//   backgroundColor: "darkblue",
+//   color: "lightblue",
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+// };
